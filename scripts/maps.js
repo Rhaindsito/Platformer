ff8f465a2f9b0bc317b3ef67bc3fd9dccb4525a5
Rhainsito <julienrhainds@gmail.com>
2025-12-07 19:02:19 -0500

    Making blocks loader basically better

--- scripts/maps.js
+++ scripts/maps.js
@@ -1,8 +1,8 @@
 const maps = [
     {
         blocks: [
-            {position: {x: 5, y: 3}, blockType: 1},
-            {position: {x: 7, y: 9}, blockType: 1}
+            {position: {x: 0, y: 11}, blockType: 1, repeat: 20, repeatDirection: "horizontal-right"},
+            {position: {x: 7, y:8}, blockType: 1, repeat: 6, repeatDirection: "horizontal-right"},
         ]
     }
 ];
--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,28 @@
-import React from "react";
-
 const gameWindow = document.getElementById("main");
 
-function loadMap(map){
-    const block = createElement('div');
-    block.classList.add("block");
-    block.appendChild(gameWindow);
+function loadBlock(block){
+    for (let j = 0; j < ((block.repeat) ? block.repeat : 1); j++){
+        const blockElement = document.createElement('div');
+        blockElement.classList.add("block");
+        gameWindow.appendChild(blockElement);
+
+        let repeatX = (block.repeatDirection == "horizontal-right") ? j : (block.repeatDirection == "horizontal-left") ? -j : 0;
+        let repeatY = (block.repeatDirection == "vertical-down") ? j : (block.repeatDirection == "vertical-up") ? -j : 0;
+        if (!block.repeatDirection){repeatX = j;}
+
+        blockElement.style.gridColumn = `${(block.position.x + 1) + repeatX} / ${(block.position.x + 2) + repeatX}`;
+        blockElement.style.gridRow = `${(block.position.y + 1) + repeatY} / ${(block.position.y + 2) + repeatY}`; 
+    }
 }
 
-document.addEventListener('DOMContentLoaded', loadMap(maps[0]));
+function loadMap(map){
+    const blocks = map.blocks;
+    console.log(blocks[0]);
+    for (let i = 0; i < blocks.length; i++){
+        loadBlock(blocks[i]);
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadMap(maps[0]);
+});
